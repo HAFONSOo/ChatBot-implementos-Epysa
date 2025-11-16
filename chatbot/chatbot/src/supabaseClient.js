e446c6ecb04aed416476e1b637750b3b05e1892e
Alonso Toledo Miranda <alon.toledo@duocuc.cl>
2025-11-16 20:23:34 -0300

    se añade documentacion fase 3

--- chatbot/chatbot/src/supabaseClient.js
+++ chatbot/chatbot/src/supabaseClient.js
@@ -1,5 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://ftwhfyigfxmdtexdoapb.supabase.co'
-const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0d2hmeWlnZnhtZHRleGRvYXBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwMjE2MjMsImV4cCI6MjA3NzU5NzYyM30.-i7niYbn5z8p8VmNcYaEgLrLlhOGJ8q6A4fA22Gbs8M'
+const supabaseUrl = 'https://iyjpcywewsbyhjkpctbc.supabase.co'
+const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5anBjeXdld3NieWhqa3BjdGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2NDMxMTQsImV4cCI6MjA3ODIxOTExNH0.c_XCzQNft1ZJCg6kpU1m5EaID0YV5dTq91RKlBrIz1o'
 export const supabase = createClient(supabaseUrl, supabaseKey)
